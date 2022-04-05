985b4ce3ba60371b680e5a84dadb94fc22ca6596
ryanadams <rcoleman.adams@gmail.com>
2022-04-05 13:10:12 -0500

    commit tree 2dc1020e83989bf9d53045e82fedafe244c54d50

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat){
+    cats.push(cat);
+    return cats;
+};
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+    return cats;
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+};
+
+function appendCat(cat){
+    let newCats = [...cats, cat];
+    return newCats;
+};
+
+function prependCat(cat){
+    let newCats = [cat, ...cats];
+    return newCats;
+};
+
+function removeLastCat(){
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats;
+};
+
+function removeFirstCat(){
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats;
+};
